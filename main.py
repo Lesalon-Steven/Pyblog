cf6b74ed28cce461c04f8d8182ae8a90aa64e85d
Lesalon_Steve... <lesalonsteven@gmail.com>
2024-04-28 18:55:31 +0200

    Mail Connection

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
 
 app = Flask(__name__)
 
 my_api = "https://api.npoint.io/4e1f43c7040a6fa538fa"
 
-
 posts = requests.get(my_api).json()
-
-
-
 
 
 @app.route('/')
@@ -22,9 +19,27 @@ def about_me():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["POST", "GET"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "GET":
+        return render_template("contact.html")
+    if request.method == "POST":
+        return data_handler()
+
+
+@app.route("/contact")
+def data_handler():
+    mail = ""
+    passkey = ""
+    name_given = request.form["name"]
+    email_given = request.form["email"]
+    message = request.form["message"]
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(mail, passkey)
+        connection.sendmail(from_addr=mail, to_addrs=email_given, msg=f"Subject:Complain\n\n {message}", )
+
+    return f"<h1>{name_given}, {email_given}</h1>"
 
 
 if __name__ == "__main__":
